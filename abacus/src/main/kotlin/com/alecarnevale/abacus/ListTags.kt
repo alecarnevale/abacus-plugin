77120abf1f556c3baa8c0589ca6f55facfa451e3
alecarnevale <alecarnevale.dev@gmail.com>
2023-03-18 15:39:55 +0100

    fetch tags before tag task

--- abacus/src/main/kotlin/com/alecarnevale/abacus/ListTags.kt
+++ abacus/src/main/kotlin/com/alecarnevale/abacus/ListTags.kt
@@ -5,10 +5,12 @@ import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.TaskProvider
 import java.io.File
 
-private const val TASK_NAME = "abacusTags"
+private const val ABACUS_TAGS_TASK_NAME = "abacusTags"
 private const val GIT_EXEC_NAME = "git"
-private const val GIT_TAG_NAME = "tag"
-private const val GIT_TAG_ORDERING_ARGS = "--sort=v:refname"
+private const val FETCH = "fetch"
+private const val FETCH_TAGS_ARG = "--tags"
+private const val GIT_TAG = "tag"
+private const val GIT_TAG_ORDERING_ARG = "--sort=v:refname"
 
 /**
  * Register and configure a task that lists all tags found in [tagsFile].
@@ -23,10 +25,16 @@ internal fun Project.listTags(tagsFile: File) {
   // Simple git plugin was introduced to simplify the usage of git through a plugin,
   // unfortunately it didn't find a solution for redirecting the output of gitTags into a file
   // but I start write raw Exec command like this, adding a dependency like simple git become pointless
-  val abacusTagsTask: TaskProvider<Exec> = project.tasks.register(TASK_NAME, Exec::class.java)
+  val abacusTagsTask: TaskProvider<Exec> = project.tasks.register(ABACUS_TAGS_TASK_NAME, Exec::class.java)
   abacusTagsTask.configure {
+    it.doFirst {
+      exec {
+        it.executable = GIT_EXEC_NAME
+        it.args = listOf(FETCH, FETCH_TAGS_ARG)
+      }
+    }
     it.executable = GIT_EXEC_NAME
-    it.args = listOf(GIT_TAG_NAME, GIT_TAG_ORDERING_ARGS)
+    it.args = listOf(GIT_TAG, GIT_TAG_ORDERING_ARG)
     it.standardOutput = tagsFile.outputStream()
   }
 }
