382f9b5ffcf39589b4d038589f8e93aa79e79583
alecarnevale <alecarnevale.dev@gmail.com>
2023-03-18 15:39:55 +0100

    abacus plugin with single or multiple counting

--- abacus/src/main/kotlin/com/alecarnevale/abacus/AbacusPlugin.kt
+++ abacus/src/main/kotlin/com/alecarnevale/abacus/AbacusPlugin.kt
@@ -6,128 +6,140 @@ import com.alecarnevale.abacus.task.CountExtensionFileTask
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.logging.LogLevel
-import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskProvider
+import xyz.ronella.gradle.plugin.simple.git.task.GitCheckout
 import java.io.File
 
 private const val SIMPLE_GIT_PLUGIN_ID = "xyz.ronella.simple-git"
 private const val OUTPUT_FILE_PATH = "build/abacus/output.csv"
-private const val GIT_TAG_ORDERING_ARGS = "--sort=v:refname"
-private const val TAGS_OUTPUT_PATH = "build/abacus/tags.txt"
-private val startingTagDefault: String? = null
+private const val DEFAULT_TAGS_FILE_PATH = "build/abacus/tags.txt"
+internal const val ABACUS_PLUGIN_EXTENSION_NAME = "abacus"
 
 class AbacusPlugin : Plugin<Project> {
+  private lateinit var pluginExtension: AbacusPluginExtension
+  private lateinit var countingOutputFile: File
+  private lateinit var tagsFile: File
+
   override fun apply(project: Project) {
-    project.pluginManager.apply(SIMPLE_GIT_PLUGIN_ID)
-    val extension = project.extensions.create("abacus", AbacusPluginExtension::class.java)
-    extension.startingTag.convention(startingTagDefault)
-    val outputFile = File(OUTPUT_FILE_PATH)
-    outputFile.delete()
-    val tagsFile = File(TAGS_OUTPUT_PATH)
-    tagsFile.delete()
+    project.setup()
 
-    project.tasks.create("countClassFile", CountClassFileTask::class.java)
-    project.tasks.create("countExtensionFile", CountExtensionFileTask::class.java)
+    project.listTags(tagsFile)
 
-    val countClassFile: TaskProvider<CountClassFileTask> = project.tasks.named("countClassFile", CountClassFileTask::class.java)
-    val countExtensionFile: TaskProvider<CountExtensionFileTask> = project.tasks.named("countExtensionFile", CountExtensionFileTask::class.java)
-
-    countClassFile.configure {
-      it.supertypes.set(extension.supertypes)
-      it.tag.set(extension.startingTag)
-      it.outputFile.set(outputFile)
-    }
-
-    countExtensionFile.configure {
-      it.fileDescriptors.set(extension.fileDescriptors)
-      it.tag.set(extension.startingTag)
-      it.outputFile.set(outputFile)
-    }
-
-    // TODO this is weird
-    // simple git plugin was introduced to simplify the usage of git through a plugin,
-    // unfortunately it didn't find a solution for redirecting the output of gitTags into a file
-    // but I start write raw Exec command like this, adding a dependency like simple git become pointless
-    project.tasks.create("exec", Exec::class.java)
-    val gitTagTask: TaskProvider<Exec> = project.tasks.named("exec", Exec::class.java)
-    gitTagTask.configure {
-      it.executable = "git"
-      it.args = listOf("tag", GIT_TAG_ORDERING_ARGS)
-      it.standardOutput = tagsFile.outputStream()
+    val tags = tagsFile.readLines()
+    val tasks = if (tags.isEmpty()) {
+      project.singleCounting()
+    } else {
+      project.multipleCounting(tags)
     }
 
     val abacusTask = project.tasks.create("abacus", AbacusTask::class.java)
-    abacusTask.dependsOn(countClassFile, countExtensionFile, gitTagTask)
+    abacusTask.dependsOn(tasks)
+  }
+
+  private fun Project.setup() {
+    project.pluginManager.apply(SIMPLE_GIT_PLUGIN_ID)
+    pluginExtension = project.extensions.create(ABACUS_PLUGIN_EXTENSION_NAME, AbacusPluginExtension::class.java)
+    pluginExtension.tagsFilePath.convention(DEFAULT_TAGS_FILE_PATH)
+    setupFiles()
+  }
+
+  private fun setupFiles() {
+    countingOutputFile = File(OUTPUT_FILE_PATH)
+    countingOutputFile.delete()
+    tagsFile = File(pluginExtension.tagsFilePath.get())
+  }
+
+  /**
+   * Counts only for the current codebase.
+   */
+  private fun Project.singleCounting(): List<TaskProvider<out Task>> {
+    project.tasks.create("countClassFile", CountClassFileTask::class.java)
+    project.tasks.create("countExtensionFile", CountExtensionFileTask::class.java)
+
+    val countClassFile: TaskProvider<CountClassFileTask> =
+      project.tasks.named("countClassFile", CountClassFileTask::class.java)
+    val countExtensionFile: TaskProvider<CountExtensionFileTask> =
+      project.tasks.named("countExtensionFile", CountExtensionFileTask::class.java)
+
+    countClassFile.configure {
+      it.supertypes.set(pluginExtension.supertypes)
+      it.outputFile.set(countingOutputFile)
+    }
+
+    countExtensionFile.configure {
+      it.fileDescriptors.set(pluginExtension.fileDescriptors)
+      it.outputFile.set(countingOutputFile)
+    }
+
+    return listOf(countClassFile, countExtensionFile)
+  }
+
+  /**
+   * Counts every tags requested.
+   */
+  private fun Project.multipleCounting(tags: List<String>): List<TaskProvider<out Task>> {
+    val resultList = mutableListOf<TaskProvider<out Task>>()
+
+    project.tasks.create("gitCheckoutMaster", GitCheckout::class.java)
+    val gitCheckoutMaster = project.tasks.named("gitCheckoutMaster", GitCheckout::class.java)
+    gitCheckoutMaster.configure {
+      it.branch.set("master")
+    }
+
+    repeat(tags.size) { index ->
+      val currentTag = tags[index]
+      val gitCheckoutTagTaskName = "gitCheckoutTag$index"
+      val countClassFileTaskName = "countClassFile$index"
+      val countExtensionsFileTaskName = "countExtensionFile$index"
+
+      project.tasks.create(gitCheckoutTagTaskName, GitCheckout::class.java)
+      project.tasks.create(countClassFileTaskName, CountClassFileTask::class.java)
+      project.tasks.create(countExtensionsFileTaskName, CountExtensionFileTask::class.java)
+
+      val gitCheckoutTag: TaskProvider<GitCheckout> =
+        project.tasks.named(gitCheckoutTagTaskName, GitCheckout::class.java)
+      gitCheckoutTag.configure {
+        it.branch.set("tags/$currentTag")
+      }
+
+      val countClassFile: TaskProvider<CountClassFileTask> =
+        project.tasks.named(countClassFileTaskName, CountClassFileTask::class.java)
+      val countExtensionFile: TaskProvider<CountExtensionFileTask> =
+        project.tasks.named(countExtensionsFileTaskName, CountExtensionFileTask::class.java)
+
+      countClassFile.configure {
+        it.supertypes.set(pluginExtension.supertypes)
+        it.tag.set(currentTag)
+        it.outputFile.set(countingOutputFile)
+      }
+
+      countExtensionFile.configure {
+        it.fileDescriptors.set(pluginExtension.fileDescriptors)
+        it.tag.set(currentTag)
+        it.outputFile.set(countingOutputFile)
+      }
+
+      countClassFile.get().dependsOn(gitCheckoutTag.get())
+      countExtensionFile.get().dependsOn(countClassFile.get())
+
+      resultList.add(countExtensionFile)
+    }
+
+    if (resultList.isNotEmpty()) {
+      gitCheckoutMaster.get().dependsOn(resultList.last())
+      resultList.add(gitCheckoutMaster)
+    }
+
+    return resultList
   }
 }
 
-abstract class AbacusTask: DefaultTask() {
+abstract class AbacusTask : DefaultTask() {
   @TaskAction
   fun starts() {
     project.logger.log(LogLevel.LIFECYCLE, "Abacus starts")
   }
 }
-
-/*
-abstract class GitTask: DefaultTask() {
-    @TaskAction
-    fun switch() {
-        val gitStatusTask: Task? = project.tasks.findByPath(":demo:gitStatus")
-        println("gitStatusTask $gitStatusTask")
-        this.dependsOn(gitStatusTask)
-    }
-}*/
-
-/*
-
-    project.tasks.create("countFile100", CountFileTask::class.java)
-    val countFile100: TaskProvider<CountFileTask> = project.tasks.named("countFile100", CountFileTask::class.java)
-    countFile100.configure {
-      it.greeting.set(extension.message)
-      it.outputFile.set(extension.outputFile)
-    }
-    project.tasks.create("countFile110", CountFileTask::class.java)
-    val countFile110: TaskProvider<CountFileTask> = project.tasks.named("countFile110", CountFileTask::class.java)
-    countFile110.configure {
-      it.greeting.set(extension.message)
-      it.outputFile.set(extension.outputFile)
-    }
-
-    project.tasks.create("gitTag100", GitCheckout::class.java)
-    val gitTag100: TaskProvider<GitCheckout> = project.tasks.named("gitTag100", GitCheckout::class.java)
-    gitTag100.configure {
-      it.doFirst {
-        extension.outputFile.get().delete()
-        extension.outputFile.get().appendText(
-          buildString {
-            append("1.0.0")
-            appendLine()
-          }
-        )
-      }
-      it.branch.set("tags/1.0.0")
-    }
-    project.tasks.create("gitTag110", GitCheckout::class.java)
-    val gitTag110: TaskProvider<GitCheckout> = project.tasks.named("gitTag110", GitCheckout::class.java)
-    gitTag110.configure {
-      it.branch.set("tags/1.1.0")
-    }
-    gitTag110.get().doFirst {
-      extension.outputFile.get().appendText(
-        buildString {
-          append("1.1.0")
-          appendLine()
-        }
-      )
-    }
-    project.tasks.create("gitRestoreMaster", GitCheckout::class.java)
-    val gitRestoreMaster: TaskProvider<GitCheckout> = project.tasks.named("gitRestoreMaster", GitCheckout::class.java)
-
-    gitRestoreMaster.get().mustRunAfter(countFile110)
-    countFile110.get().mustRunAfter(gitTag110)
-    gitTag110.get().mustRunAfter(countFile100)
-    countFile100.get().mustRunAfter(gitTag100)
-    gitRestoreMaster.get().dependsOn(gitTag110, gitTag100, countFile110, countFile100)
- */
